7de66faa973759eec62fe0e2f681583c8d275735
DVictory <derrickfinch35@gmail.com>
2018-12-08 12:16:30 -0500

    1

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friendMatch = [
+    {
+      name: "Stephen A Smith",
+      photo: "https://cdn1.thecomeback.com/wp-content/uploads/sites/94/2016/02/Stephen-A-Smith-sour-face.jpg",
+      scores: ["2", "3", "2", "1", "2"]
+    }, {
+      name: "Gwen Stefani",
+      photo: "https://scstylecaster.files.wordpress.com/2018/07/gwen-stefani.jpg",
+      scores: ["3", "2", "1", "3", "2"]
+    }, {
+      name: "Kobe Bryant",
+      photo: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2016/08/22/103884430-GettyImages-139369178.1910x1000.jpg",
+      scores: ["3", "2", "2", "2", "2"]
+    }, {
+      name: "Kanye West",
+      photo: "https://www.biography.com/.image/t_share/MTU0OTkwNDUxOTQ5MDUzNDQ3/kanye-west-attends-the-christian-dior-show-as-part-of-the-paris-fashion-week-womenswear-fall-winter-2015-2016-on-march-6-2015-in-paris-france-photo-by-dominique-charriau-wireimage-square.jpg",
+      scores: ["2", "3", "3", "1", "2"]
+    }, {
+    name: "Martin Lawrence",
+    photo: "https://www.nydailynews.com/resizer/mBLRMYd-jYOzhW2K_wOkcHgUers=/1400x0/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/T53NUE5HWP2ODZOKTBEDMARRKY.jpg",
+    scores: ["3", "1", "2", "2", "2"]
+    }
+  
+  ];
+  
+  
+  
+  module.exports = friendMatch;
